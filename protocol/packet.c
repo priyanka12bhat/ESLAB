6d39c20fbc8820f017a5a32e59fb487462f2b3c5
Arun Geo Thomas <arungeothomas@gmail.com>
2018-06-04 20:03:26 +0200

    Revert "Barometer Telemetry Update + Improvizations"
    This reverts commit cd82886b642142b5f7066c788ae3469bea8818d0.

--- protocol/packet.c
+++ protocol/packet.c
@@ -70,45 +70,41 @@ unsigned char *Get_Byte_Stream(Packet *pkt)
 }
 
 
-Packet *Create_Telemetery_Packet(uint16_t bat_volt, int16_t *MotorValues, int16_t phi, int16_t theta, int16_t psi, int16_t sp, int16_t sq, int16_t sr, unsigned char _msgCode, int16_t PArray[], uint32_t pressure)
+Packet *Create_Telemetery_Packet(uint16_t bat_volt, int16_t *MotorValues, int16_t phi, int16_t theta, int16_t psi, int16_t sp, int16_t sq, int16_t sr, unsigned char _msgCode, int16_t PArray[])
 {
 	TpktObj.startByte = START_BYTE;
 	TpktObj.type = T_DATA;
-	unsigned char* charTemp = (unsigned char* )MotorValues;
-	for(uint8_t i =0;i<8;i++)
+	for(uint8_t i =0;i<4;i++)
 	{
-		TpktObj.value[i]=charTemp[i];
-		
+		TpktObj.value[(i<<1)]=MotorValues[i]>>8;
+		TpktObj.value[(i<<1)+1]=MotorValues[i]&0x00FF;
 	}
 
-	//REMINDER: Consider Endianess
-	TpktObj.value[9]=bat_volt>>8;
-	TpktObj.value[8]=bat_volt&0x00FF;
+	TpktObj.value[8]=bat_volt>>8;
+	TpktObj.value[9]=bat_volt&0x00FF;
 
-	TpktObj.value[11]=phi>>8;
-	TpktObj.value[10]=phi&0x00FF;
-	TpktObj.value[13]=theta>>8;
-	TpktObj.value[12]=theta&0x00FF;
-	TpktObj.value[15]=psi>>8;
-	TpktObj.value[14]=psi&0x00FF;
-	TpktObj.value[17]=sp>>8;
-	TpktObj.value[16]=sp&0x00FF;
-	TpktObj.value[19]=sq>>8;
-	TpktObj.value[18]=sq&0x00FF;
-	TpktObj.value[21]=sr>>8;
-	TpktObj.value[20]=sr&0x00FF;
+	TpktObj.value[10]=phi>>8;
+	TpktObj.value[11]=phi&0x00FF;
+	TpktObj.value[12]=theta>>8;
+	TpktObj.value[13]=theta&0x00FF;
+	TpktObj.value[14]=psi>>8;
+	TpktObj.value[15]=psi&0x00FF;
+	TpktObj.value[16]=sp>>8;
+	TpktObj.value[17]=sp&0x00FF;
+	TpktObj.value[18]=sq>>8;
+	TpktObj.value[19]=sq&0x00FF;
+	TpktObj.value[20]=sr>>8;
+	TpktObj.value[21]=sr&0x00FF;
 	TpktObj.value[22]=_msgCode;
-	charTemp = (unsigned char* )PArray;
-	for(uint8_t i =0;i<6;i++)
-	{
-		TpktObj.value[23+i]=charTemp[i];
-		
-	}
-	TpktObj.value[29]=(pressure>>16)&0xFF;
-	TpktObj.value[30]=(pressure>>8)&0xFF;
-	TpktObj.value[31]=pressure&0xFF;
+	TpktObj.value[23]=PArray[0]>>8;
+	TpktObj.value[24]=PArray[0]&0x00FF;
+	TpktObj.value[25]=PArray[1]>>8;
+	TpktObj.value[26]=PArray[1]&0x00FF;
+	TpktObj.value[27]=PArray[2]>>8;
+	TpktObj.value[28]=PArray[2]&0x00FF;	
 
-	TpktObj.valueLength = 32;
+
+	TpktObj.valueLength = 29;
 	TpktObj.dataLength=Get_DataLength(TpktObj.valueLength );
 	TpktObj.packetLength=1+1+TpktObj.dataLength+(CRC_BYTE_SIZE);
 	Set_CRCValue(&TpktObj);
