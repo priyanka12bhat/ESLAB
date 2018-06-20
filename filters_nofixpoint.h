7ca43706268c3c941f8a760f4b3b1cf372f617ae
MingxiLi <32333706+MingxiLi@users.noreply.github.com>
2018-06-20 10:41:27 +0200

    kalman without fixpoint

--- filters_nofixpoint.h
+++ filters_nofixpoint.h
@@ -1,20 +1,18 @@
 #define BUTTER_B0 16384
-#define BUTTER_B1 -31303
-#define BUTTER_B2 14981
-#define BUTTER_A0 15
-#define BUTTER_A1 31
-#define BUTTER_A2 15
+#define BUTTER_B1 -29243
+#define BUTTER_B2 13203
+#define BUTTER_A0 85
+#define BUTTER_A1 172
+#define BUTTER_A2 85
 
-#define KALMAN_P2PHI_THETA 0.001
+#define KALMAN_P2PHI_THETA 0.0025
 #define KALMAN_C1_THETA 60000
 #define KALMAN_C2_THETA 3000000000
 
-#define KALMAN_P2PHI_PHI 0.001
+#define KALMAN_P2PHI_PHI 0.0025
 #define KALMAN_C1_PHI 60000
 #define KALMAN_C2_PHI 3000000000
 
-#define normal2fixM(x)((q14)(x)*(1<<Q));
-#define fix2normalM(x)(x/(1<<Q));
 
 void butterworth();
 void kalman();
